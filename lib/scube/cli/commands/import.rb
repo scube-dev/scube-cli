489a948296e46034d05009c2b71565e4db369607
Thibault Jouan <tj@a13.fr>
2015-05-02 15:03:16 +0000

    Check `import' command arguments

--- lib/scube/cli/commands/import.rb
+++ lib/scube/cli/commands/import.rb
@@ -3,10 +3,47 @@ module Scube
     module Commands
       class Import
         def initialize args
-          @arguments = args
+          @client = Client.new
+          @stats  = {
+            created: {
+              count:  0,
+              size:   0
+            },
+            ignored: {
+              count:  0,
+              size:   0
+            }
+          }
         end
 
         def run
+          ARGF.each do |l|
+            path    = Pathname.new(l.chomp)
+            digest  = Digest::SHA256.file(path)
+
+            if @client.sound? digest
+              increment_stat :ignored, path
+            else
+              @client.sound_post path
+              increment_stat :created, path
+            end
+          end
+
+          puts "#{stats_report_text} in X seconds"
+        end
+
+
+        private
+
+        def increment_stat stat, path
+          @stats[stat][:count]  += 1
+          @stats[stat][:size]   += path.size
+        end
+
+        def stats_report_text
+          @stats.keys.each_with_object [] do |e, m|
+            m << "#{@stats[e][:count]} #{e} (#{@stats[e][:size]} bytes)"
+          end.join ', '
         end
       end
     end
