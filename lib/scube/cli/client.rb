2e30c7103f8037aed80a6aaa3984d453f84aff1f
Thibault Jouan <tj@a13.fr>
2015-05-05 05:04:14 +0000

    Prototype client credentials handling

--- lib/scube/cli/client.rb
+++ lib/scube/cli/client.rb
@@ -5,6 +5,8 @@ module Scube
     class Client
       # FIXME: extract as CLI option with default value
       SCUBE_BASE_URI = ENV['SCUBE_BASE_URI'].dup.freeze
+      # FIXME: extract as CLI option with default value
+      TOKEN_PATH = '~/.scube/credentials'
 
       HEADERS = {
         'Accept'        => 'application/json',
@@ -16,7 +18,10 @@ module Scube
       end
 
       def conn
-        @conn ||= Faraday.new(@base_uri, headers: HEADERS)
+        @conn ||= Faraday.new(@base_uri, headers: HEADERS).tap do |c|
+          c.authorization :Token,
+            token: File.read(File.expand_path(TOKEN_PATH)).chomp
+        end
       end
 
       def ping
